54b2304afe28bf8f710090d20a41d3ea6dd3c576
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-10-21 13:12:06 -0400

    update
    print AFC and NFC teams is now working. now need to fix the menu and clean up code.

--- lib/app/cli.rb
+++ lib/app/cli.rb
@@ -2,7 +2,6 @@ class CLI
     def run
         puts "NFL Team Checker"
         Scraper.new.first_scrape
-        
         puts "What you you like view?"
         puts "1. NFL Teams"
         puts "2. List teams by Conference"
@@ -20,29 +19,11 @@ class CLI
             puts "2. American Football Conference"
             puts "Enter 1 or 2"
             user_input = gets.chomp
-            
-            #binding.pry
-
             if user_input == "1"
-                nfc_teams << NFLTeam.all.select do |team|
-                team.conference.name == "NFC Team"          
-               end 
-                nfc_teams.each_with_index do |team, index|
-                    puts "#{index} #{team.full_name}"
-                    puts "Enter team number to get more info about the team"   
-                end
-                    team_select = gets.chomp
-                    select_index = team_select.to_i - 1
-                    NFLTeam.nfc_teams[select_index].print_team_info
+                NFLTeam.print_nfc
             elsif user_input == "2"
-                puts 'American Football Conference'
-                afc_conference = NFLTeam.all.select do |team|
-                    team.conference.name == "AFC Team"
-                end
-            end
-        else
-            # make a way to return to top if not a valid pick
+                NFLTeam.print_afc 
+            end   
         end
-
-    end
+    end  
 end
--- lib/app/nflteam.rb
+++ lib/app/nflteam.rb
@@ -1,11 +1,11 @@
 class NFLTeam
 
     @@all = []
-
     @@nfc_teams = []
+    @@afc_teams = []
+    
     attr_accessor :short_name, :full_name, :win_count, :lose_count, :tie_count, :percentage, :record, :standing, :owners, :coach, :stadium, :next_game, :established, :conference, :division
    
-
     def initialize(short_name, full_name, win_count, tie_count, lose_count, percentage, team_url, conference_instance)
         @short_name = short_name
         @full_name = full_name
@@ -15,15 +15,19 @@ class NFLTeam
         @percentage = percentage
         @team_url = team_url
         @conference = conference_instance
-        save
-    end
-
-    def save
-       @@all << self
+        @@all << self
     end
 
     def self.all
         @@all
+    end
+
+    def self.nfc_teams
+        @@nfc_teams
+    end
+
+    def self.afc_teams
+        @@afc_teams
     end
     
     def team_record
@@ -34,24 +38,41 @@ class NFLTeam
         puts "NFL Teams"
         @@all.each_with_index do |team, index|
             puts "#{index+1} #{team.team_record}"
-           
-            
+            puts "******************************"
         end
     end
 
-    # def self.print_nfc
-    #     puts "National Football Conference"
-    #     binding.pry
-        
+    def self.print_nfc
+        NFLTeam.all.select do |team|
+        NFLTeam.nfc_teams << team if team.conference.name == "NFC Team"
+        end 
+            puts "National Football League"
+            puts "_________________________________"
+        @@nfc_teams.each_with_index do |team, index|
+            
+            puts "#{index+1} #{team.team_record}"
+            puts "_________________________________"
+        end
+            puts "Enter team number to get more info about the team"
+            team_select = gets.chomp
+            select_index = team_select.to_i - 1
+            NFLTeam.nfc_teams[select_index].print_team_info   
+    end
+
+    def self.print_afc
+            NFLTeam.all.select do |team|
+            NFLTeam.afc_teams << team if team.conference.name == "AFC Team"
+            end 
+            @@afc_teams.each_with_index do |team, index|
+                puts "#{index+1} #{team.team_record}"
+                puts "******************************"
+            end
+                puts "Enter team number to get more info about the team"
+                team_select = gets.chomp
+                select_index = team_select.to_i - 1
+                NFLTeam.afc_teams[select_index].print_team_info 
+        end
     
-
-
-    #     # each_with_index do |team, index|
-    #     #     puts "#{index+1}" 
-    #     #     puts "#{team.team_record}"
-    #end
-        
-        
     def print_team_info
         check_team_info
         puts "#{full_name}: #{@standing} -- #{@record} "
@@ -67,5 +88,11 @@ class NFLTeam
             hash = Scraper.new.second_scrape(self, @team_url)
         end
     end  
+
+   
+
 end
+
+
+
 
